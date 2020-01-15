a3e40162bf54a1f2435aa7ee938abebfeb327343
nohsikwan <tlrhks2869@naver.com>
2020-01-15 16:21:16 +0900

    controller_signup_delete_logout_db

--- controllers.js
+++ controllers.js
@@ -1,21 +1,69 @@
-//const { user } = require('./models');
+const { User, Todo } = require('./models');
 module.exports = {
-  logInController: (req, res) => {
-    // TODO : 로그인 및 인증 부여 로직 작성
-  },
-  logOutController: (req, res) => {
-    // TODO : 로그인 및 인증 부여 로직 작성
-  },
-  signUpController: (req, res) => {
-    // TODO : 회원가입 로직 및 유저 생성 로직 작성
-  },
-  todoPageContoroller: (req, res) => {
-    // TODO : 유저 todolist 요청
-  },
-  myPageContoroller: (req, res) => {
-    // TODO : 유저 회원정보 요청 로직 작성
-  }
-    //inFoContoroller: (req, res) => {
-    // TODO : 유저 todolist 요청
-  //}
+	logInController: (req, res) => {
+		// TODO : 로그인 및 인증 부여 로직 작성
+	},
+	logOutController: (req, res) => {
+		// TODO : 로그인 및 인증 부여 로직 작성
+		const { session } = req;
+
+		if (session.userid) {
+			session.destroy(err => {
+				if (err) {
+					res.status(500).send(err);
+				} else {
+					res.redirect('/');
+				}
+			});
+		} else {
+			res.redirect('/');
+		}
+	},
+
+	signUpController: (req, res) => {
+		// TODO : 회원가입 로직 및 유저 생성 로직 작성
+		const { username, email, password } = req.body;
+
+		User.findOrCreate({
+			where: {
+				username: username,
+				email: email,
+				password: password
+			}
+		})
+			.then(async ([user, created]) => {
+				if (!created) {
+					return res.status(409).send('email exists');
+				}
+				return res.status(201).send(user);
+			})
+			.catch(error => {
+				console.log(error);
+				res.sendStatus(500);
+			});
+	},
+	todoPageController: (req, res) => {
+		// TODO : 유저 todolist 요청
+	},
+	myPageController: (req, res) => {
+		// TODO : 유저 회원정보 요청 로직 작성
+	},
+	todoAdd: (req, res) => {
+		//TODO: todo 추가
+	},
+	todoEdit: (req, res) => {
+		//TODO: todo 변경
+	},
+	todoDelete: (req, res) => {
+		//TODO: todo삭제
+		const { clickedTodoItem } = req.body;
+
+		Todo.destroy({ where: { todoitem: clickedTodoItem } })
+			.then(result => {
+				res.json({});
+			})
+			.catch(err => {
+				console.error(err);
+			});
+	}
 };
--- models/todo.js
+++ models/todo.js
@@ -1,12 +1,25 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
-  const Todo = sequelize.define('Todo', {
-    todoid: DataTypes.STRING,
-    todoitem: DataTypes.STRING,
-    status: DataTypes.STRING
-  }, {});
-  Todo.associate = function(models) {
-    // associations can be defined here
-  };
-  return Todo;
+	const Todo = sequelize.define(
+		'Todo',
+		{
+			todoid: {
+				type: Sequelize.INTEGER,
+				references: 'User', // <<< Note, its table's name, not object name
+				referencesKey: 'id' // <<< Note, its a column name
+			},
+
+			todoitem: DataTypes.STRING,
+			status: DataTypes.STRING
+		},
+		{}
+	);
+	Todo.associate = function(models) {
+		// associations can be defined here
+		Todo.belongsTo(models.User, {
+			foreignKey: 'id'
+		});
+	};
+	return Todo;
 };
