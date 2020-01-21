6fd8c3fedd19708ac8aa1fa3c80e3947130e53da
nohsikwan <tlrhks2869@naver.com>
2020-01-21 16:28:44 +0900

    todoInfo_todoStatus_todoPlusSchema

--- controllers.js
+++ controllers.js
@@ -1,155 +1,167 @@
-const { Todo, User } = require("./models");
+const { Todo, User } = require('./models');
 module.exports = {
-  logInController: (req, res) => {
-    // TODO : 로그인 및 인증 부여 로직 작성
-    const { email, password } = req.body;
-    const session = req.session;
+	logInController: (req, res) => {
+		// TODO : 로그인 및 인증 부여 로직 작성
+		const { email, password } = req.body;
+		const session = req.session;
 
-    user
-      .findOne({
-        where: {
-          email: email,
-          password: password
-        }
-      })
-      .then(data => {
-        if (!data) {
-          return res.status(404).send('invalid user');
-        }
-        session.userid = data.id;
-        res.status(200).json({
-          id: data.id
-        });
-      })
-      .catch(err => {
-        res.status(404).send(err);
-      });
-  },
+		User.findOne({
+			where: {
+				email: email,
+				password: password
+			}
+		})
+			.then(data => {
+				if (!data) {
+					return res.status(404).send('invalid user');
+				}
+				session.userid = data.userid;
+				res.status(200).json({
+					id: data.userid
+				});
+			})
+			.catch(err => {
+				res.status(404).send(err);
+			});
+	},
 
+	logOutController: (req, res) => {
+		// TODO : 로그아웃  세션 제거해주기;
+		const { session } = req;
 
-  logOutController: (req, res) => {
-    // TODO : 로그인 및 인증 부여 로직 작성
-    const { session } = req;
+		if (session.userid) {
+			session.destroy(err => {
+				if (err) {
+					res.status(500).send(err);
+				} else {
+					res.redirect('/');
+				}
+			});
+		} else {
+			res.redirect('/');
+		}
+	},
 
-    if (session.userid) {
-      session.destroy(err => {
-        if (err) {
-          res.status(500).send(err);
-        } else {
-          res.redirect("/");
-        }
-      });
-    } else {
-      res.redirect("/");
-    }
-  },
+	signUpController: (req, res) => {
+		// TODO : 회원가입 로직 및 유저 생성 로직 작성
+		const { username, email, password } = req.body;
+		User.findOrCreate({
+			where: {
+				username: username,
+				email: email,
+				password: password
+			}
+		})
+			.then(async ([user, created]) => {
+				if (!created) {
+					return res.status(409).send('email exists');
+				}
+				return res.status(201).send(user);
+			})
+			.catch(error => {
+				console.log(error);
+				res.sendStatus(500);
+			});
+	},
 
-  signUpController: (req, res) => {
-    // TODO : 회원가입 로직 및 유저 생성 로직 작성
-    const { username, email, password } = req.body;
-    User.findOrCreate({
-      where: {
-        username: username,
-        email: email,
-        password: password
-      }
-    })
-      .then(async ([user, created]) => {
-        if (!created) {
-          return res.status(409).send("email exists");
-        }
-        return res.status(201).send(user);
-      })
-      .catch(error => {
-        console.log(error);
-        res.sendStatus(500);
-      });
-  },
+	todoPageContoroller: (req, res) => {
+		// TODO : 유저 todolist 요청
+		const session = req.session;
+		if (session.userid) {
+			Todo.findAll({ where: { todoid: session.userid } })
+				.then(datas => {
+					return res.status(200).json(datas);
+				})
+				.catch(err => {
+					console.log(err);
+					res.sendStatus(500);
+				});
+		}
+	},
 
-  todoPageContoroller: (req, res) => {
-    // TODO : 유저 todolist 요청
-    const session = req.session;
-    if (session.userid) {
-      Todo.findAll({ where: { todoid: session.userid } })
-        .then(datas => {
-          return res.status(200).json(datas);
-        })
-        .catch(err => {
-          console.log(err);
-          res.sendStatus(500);
-        });
-    }
-  },
+	myPageContoroller: (req, res) => {
+		// TODO : 유저 회원정보 요청 로직 작성 //상훈
+		// TODO : 유저 회원정보 요청 로직 작성
+		const session = req.session;
+		User.findOne({ where: { id: session.userid } })
+			.then(userData => {
+				Todo.count({ where: { todoid: session.userid } }).then(todoCount => {
+					Todo.count({
+						where: { todoid: session.userid, status: 'complete' }
+					}).then(completeCount => {
+						let data = {
+							userinfo: userData,
+							todoCount: todoCount,
+							completeCount: completeCount
+						};
+						res.status(200).json(data);
+					});
+				});
+			})
+			.catch(err => {
+				console.log(err);
+				res.sendStatus(500);
+			});
+	},
 
-  myPageContoroller: (req, res) => {
-    // TODO : 유저 회원정보 요청 로직 작성 //상훈
-        // TODO : 유저 회원정보 요청 로직 작성
-        const session = req.session;
-        User.findOne({ where: { id: session.userid } })
-          .then(userData => {
-            Todo.count({ where: { todoid: session.userid } }).then(todoCount => {
-              Todo.count({
-                where: { todoid: session.userid, status: "complete" }
-              }).then(completeCount => {
-                let data = {
-                  userinfo: userData,
-                  todoCount: todoCount,
-                  completeCount: completeCount
-                };
-                res.status(200).json(data);
-              });
-            });
-          })
-          .catch(err => {
-            console.log(err);
-            res.sendStatus(500);
-          });
-  },
+	todoAdd: (req, res) => {
+		//TODO: todo 추가
+		const { userid, todoid, todoitem, status } = req.body;
+		Todo.create({
+			userid: userid,
+			todoid: todoid,
+			todoitem: todoitem,
+			status: status
+		})
+			.then(el => {
+				res.status(200).send('created');
+			})
+			.catch(err => {
+				console.error(err);
+			});
+	},
+	todoDelete: (req, res) => {
+		//TODO: todo삭제
+		const { todoid } = req.body;
 
-  todoEdit: (req, res, next) => {
-    //TODO: todo 변경 //엄
-    const { todoid, todoitem } = req.body;
-    Todo.update({
-      todoid: todoid,
-      todiitem: todoitem,
-      status: "incomplete"
-    },
-    {where : req.params.userid})
-    .then(function(rowsUpdated) {
-      res.json(rowsUpdated)
-    })
-    .catch(next)
-  },
-  // Book.update(
-  //   {title: req.body.title},
-  //   {where: req.params.bookId}
-  // )
-  // .then(function(rowsUpdated) {
-  //   res.json(rowsUpdated)
-  // })
-  // .catch(next)
+		Todo.destroy({ where: { todoid: todoid } })
+			.then(result => {
+				res.json({});
+			})
+			.catch(err => {
+				console.error(err);
+			});
+	},
+	todoInfo: (req, res) => {
+		// TODO: todoid를 받아오면 해당 데이터를 보내주는 api
+		const { todoid } = req.body;
+		Todo.findOne({
+			where: {
+				todoid: todoid
+			}
+		})
+			.then(data => {
+				if (!data) {
+					return res.status(404).send('invalid todoData');
+				}
+				res.status(200).json({
+					data: data
+				});
+			})
+			.catch(err => {
+				res.status(404).send(err);
+			});
+	},
+	todoStatusEdit: (req, res) => {
+		// 받아온 새로운 값을 업데이트 해주는  todoapi
+		const { todoid, status } = req.body;
 
-  todoAdd: (req, res) => {
-    //TODO: todo 추가
-    const { todoid, todoitem } = req.body;
-    Todo.create({
-      todoid: todoid,
-      todoitem: todoitem,
-      status: "incomplete"
-    }).then(el => {
-      res.status(200).send("created");
-    });
-  },
-  todoDelete: (req, res) => {
-    //TODO: todo삭제
-    const { clickedTodoItem } = req.body;
-
-    Todo.destroy({ where: { todoitem: clickedTodoItem } })
-      .then(result => {
-        res.json({});
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  }
+		Todo.update({ status: status }, { where: { todoid: todoid } })
+			.then(res => {
+				res.status(200).send('info status  update!');
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}
 };
--- index.js
+++ index.js
@@ -7,21 +7,22 @@ const cors = require('cors');
 const app = express();
 const port = 4000;
 const {
-    logInController,
-    logOutController,
-    signUpController,
-    myPageController,
-    todoPageController,
-    todoAdd,
-    todoEdit,
-    todoDelete
+	logInController,
+	logOutController,
+	signUpController,
+	myPageController,
+	todoPageController,
+	todoAdd,
+	todoInfo,
+	todoDelete,
+	todoStatusEdit
 } = require('./controllers');
 app.use(
-    session({
-        secret: 'secret',
-        resave: false,
-        saveUninitialized: true
-    })
+	session({
+		secret: 'secret',
+		resave: false,
+		saveUninitialized: true
+	})
 );
 //세션을 사용하는 형식임
 app.use(bodyParser.json());
@@ -29,42 +30,25 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 //모건을 사용하는 형식
 app.use(
-    cors({
-        origin: ['http://localhost:3000'],
-        method: ['GET', 'POST', 'PUT', 'DELETE'],
-        credentials: true
-    })
+	cors({
+		origin: ['http://localhost:3000'],
+		method: ['GET', 'POST', 'PUT', 'DELETE'],
+		credentials: true
+	})
 );
-//코어스를 사용하는 형식
-app.get('/user', () => {
-    console.log('helloworld');
-});
-/*
+// 코어스를 사용하는 형식
+
 app.post('/user/login', logInController);
 app.post('/user/signup', signUpController);
 app.get('/user/logout', logOutController);
 app.get('/user/mypage', myPageController);
 app.get('/user/todopage', todoPageController);
 app.post('/todo/add', todoAdd);
-app.put('/todo/edit', todoEdit);
+app.post('/todo/info', todoInfo);
+app.post('/todo/status', todoStatusEdit);
 app.delete('/todo/delete', todoDelete);
-*/
+
 app.listen(port, () => {
-    console.log(`server listen on ${port}`);
+	console.log(`server listen on ${port}`);
 });
 module.exports = app;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
