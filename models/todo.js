8d4a00a3a28bafae9a1f9fe713b7ab9deec51918
sangelkim89 <sangelkim89@gmail.com>
2020-01-21 17:02:53 +0900

    to pull from upstream

--- models/todo.js
+++ models/todo.js
@@ -4,12 +4,7 @@ module.exports = (sequelize, DataTypes) => {
   const Todo = sequelize.define(
     "Todo",
     {
-      todoid: {
-        type: Sequelize.INTEGER,
-        references: "User", // <<< Note, its table's name, not object name
-        referencesKey: "id" // <<< Note, its a column name
-      },
-
+      todoid: DataTypes.STRING,
       todoitem: DataTypes.STRING,
       status: DataTypes.STRING
     },
@@ -17,9 +12,9 @@ module.exports = (sequelize, DataTypes) => {
   );
   Todo.associate = function(models) {
     // associations can be defined here
-    Todo.belongsTo(models.User, {
-      foreignKey: "id"
-    });
+    // Todo.belongsTo(models.User, {
+    //   foreignKey: "id"
+    // });
   };
   return Todo;
 };
